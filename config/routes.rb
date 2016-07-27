3f54bbde7a2404bbef76ed5b507a11530a5d8d83
ibo <ibo.b@tcs.com>
2016-07-27 13:24:15 -0400

    Add message controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   root 'rooms#index'
   resources :rooms, only: [:index, :show]
+  resources :messages, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Room.delete_all
 rooms = Room.create([{name: 'Hang Out'},{name: 'Chill Out'}])
+User.delete_all
+users = User.create([{name: 'Ibo', email: 'ibo@gmail.com', password: 'password'},
+                     {name: 'Gabriel', email: 'gabriel@gmail.com', password: 'password'},
+                     {name: 'Johanna', email: 'johanna@gmail.com', password: 'password'}])
